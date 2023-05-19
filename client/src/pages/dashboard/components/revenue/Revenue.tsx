85547c96a2694d95cd7fe464e209a34f1564e059
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2023-05-18 21:44:15 -0300

    tab

--- client/src/pages/dashboard/components/revenue/Revenue.tsx
+++ client/src/pages/dashboard/components/revenue/Revenue.tsx
@@ -19,47 +19,47 @@ const Revenue = ({data}: IRevenue) => {
 
   return (
     <ResponsiveContainer width="100%" height="100%">
-    <BarChart
-      width={500}
-      height={300}
-      data={revenue}
-      margin={{
-        top: 17,
-        right: 15,
-        left: -5,
-        bottom: 58,
-      }}
-    >
-      <defs>
-        <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
-          <stop
-            offset="5%"
-            stopColor={palette.primary[300]}
-            stopOpacity={0.8}
-          />
-          <stop
-            offset="95%"
-            stopColor={palette.primary[300]}
-            stopOpacity={0}
-          />
-        </linearGradient>
-      </defs>
-      <CartesianGrid vertical={false} stroke={palette.grey[800]} />
-      <XAxis
-        dataKey="name"
-        axisLine={false}
-        tickLine={false}
-        style={{ fontSize: "10px" }}
-      />
-      <YAxis
-        axisLine={false}
-        tickLine={false}
-        style={{ fontSize: "10px" }}
-      />
-      <Tooltip />
-      <Bar dataKey="revenue" fill="url(#colorRevenue)" />
-    </BarChart>
-  </ResponsiveContainer>
+      <BarChart
+        width={500}
+        height={300}
+        data={revenue}
+        margin={{
+          top: 17,
+          right: 15,
+          left: -5,
+          bottom: 58,
+        }}
+      >
+        <defs>
+          <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+            <stop
+              offset="5%"
+              stopColor={palette.primary[300]}
+              stopOpacity={0.8}
+            />
+            <stop
+              offset="95%"
+              stopColor={palette.primary[300]}
+              stopOpacity={0}
+            />
+          </linearGradient>
+        </defs>
+        <CartesianGrid vertical={false} stroke={palette.grey[800]} />
+        <XAxis
+          dataKey="name"
+          axisLine={false}
+          tickLine={false}
+          style={{ fontSize: "10px" }}
+        />
+        <YAxis
+          axisLine={false}
+          tickLine={false}
+          style={{ fontSize: "10px" }}
+        />
+        <Tooltip />
+        <Bar dataKey="revenue" fill="url(#colorRevenue)" />
+      </BarChart>
+    </ResponsiveContainer>
   )
 }
 
--- client/src/pages/dashboard/components/revenueExpenses/RevenueExpenses.tsx
+++ client/src/pages/dashboard/components/revenueExpenses/RevenueExpenses.tsx
@@ -18,73 +18,73 @@ const RevenueExpenses = ({data}: IRevenueExpenses) => {
 
   return (
     <ResponsiveContainer width="100%" height="100%">
-          <AreaChart
-            width={500}
-            height={400}
-            data={revenueExpenses}
-            margin={{
-              top: 15,
-              right: 25,
-              left: -10,
-              bottom: 60,
-            }}
-          >
-            <defs>
-              <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor={palette.primary[300]}
-                  stopOpacity={0.5}
-                />
-                <stop
-                  offset="95%"
-                  stopColor={palette.primary[300]}
-                  stopOpacity={0}
-                />
-              </linearGradient>
-              <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor={palette.primary[300]}
-                  stopOpacity={0.5}
-                />
-                <stop
-                  offset="95%"
-                  stopColor={palette.primary[300]}
-                  stopOpacity={0}
-                />
-              </linearGradient>
-            </defs>
-            <XAxis
-              dataKey="name"
-              tickLine={false}
-              style={{ fontSize: "10px" }}
+      <AreaChart
+        width={500}
+        height={400}
+        data={revenueExpenses}
+        margin={{
+          top: 15,
+          right: 25,
+          left: -10,
+          bottom: 60,
+        }}
+      >
+        <defs>
+          <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+            <stop
+              offset="5%"
+              stopColor={palette.primary[300]}
+              stopOpacity={0.5}
             />
-            <YAxis
-              tickLine={false}
-              axisLine={{ strokeWidth: "0" }}
-              style={{ fontSize: "10px" }}
-              domain={[8000, 23000]}
+            <stop
+              offset="95%"
+              stopColor={palette.primary[300]}
+              stopOpacity={0}
             />
-            <Tooltip />
-            <Area
-              type="monotone"
-              dataKey="revenue"
-              dot={true}
-              stroke={palette.primary.main}
-              fillOpacity={1}
-              fill="url(#colorRevenue)"
+          </linearGradient>
+          <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
+            <stop
+              offset="5%"
+              stopColor={palette.primary[300]}
+              stopOpacity={0.5}
             />
-            <Area
-              type="monotone"
-              dataKey="expenses"
-              dot={true}
-              stroke={palette.primary.main}
-              fillOpacity={1}
-              fill="url(#colorExpenses)"
+            <stop
+              offset="95%"
+              stopColor={palette.primary[300]}
+              stopOpacity={0}
             />
-          </AreaChart>
-        </ResponsiveContainer>
+          </linearGradient>
+        </defs>
+        <XAxis
+          dataKey="name"
+          tickLine={false}
+          style={{ fontSize: "10px" }}
+        />
+        <YAxis
+          tickLine={false}
+          axisLine={{ strokeWidth: "0" }}
+          style={{ fontSize: "10px" }}
+          domain={[8000, 23000]}
+        />
+        <Tooltip />
+        <Area
+          type="monotone"
+          dataKey="revenue"
+          dot={true}
+          stroke={palette.primary.main}
+          fillOpacity={1}
+          fill="url(#colorRevenue)"
+        />
+        <Area
+          type="monotone"
+          dataKey="expenses"
+          dot={true}
+          stroke={palette.primary.main}
+          fillOpacity={1}
+          fill="url(#colorExpenses)"
+        />
+      </AreaChart>
+    </ResponsiveContainer>
   )
 }
 
